4ac41cbe04b107e577a755910645be71fdce4762
Aanand Deva Suresh <emailcrystalfield@gmail.com>
2023-11-17 10:41:46 +0530

    ggfg

--- BPP/client/dist/schemas/callbacks/request.callback.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestCallbackSchema = void 0;
-const zod_1 = require("zod");
-exports.requestCallbackSchema = zod_1.z.object({
-    context: zod_1.z.any(),
-    message: zod_1.z.any()
-});
--- README.md
+++
@@ -1,1 +0,0 @@
-# karma-net
